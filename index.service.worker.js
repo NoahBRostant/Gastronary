2f8a9ab9e1fc2b750557d325fb15aba480a356f7
NoahBRostant <noah.b.rostant@gmail.com>
2025-03-22 13:25:26 +0000

    fixed main scene bug

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1742649389|7178441547';
+const CACHE_VERSION = '1742649897|7686334785';
 /** @type {string} */
 const CACHE_PREFIX = 'index-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
