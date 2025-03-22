5b13686c86f412ccd49f2016c95f5868280b902a
NoahBRostant <noah.b.rostant@gmail.com>
2025-03-22 23:01:17 +0000

    Added a whole load of shtuff

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1742649897|7686334785';
+const CACHE_VERSION = '1742684392|16373213758';
 /** @type {string} */
 const CACHE_PREFIX = 'index-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
